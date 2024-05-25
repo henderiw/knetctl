133a42a55f6b1392edf04292d312f80ed2c657fd
Wim Henderickx <wim.henderickx@gmail.com>
2024-05-25 06:13:41 +0200

    added install with new repo

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@
 : ${USE_PKG:="false"} # default --use-pkg flag value. will use package installation by default unless the default is changed to false
 : ${VERIFY_CHECKSUM:="false"}
 : ${BIN_INSTALL_DIR:="/usr/local/bin"}
-: ${REPO_NAME:="kubenet-dev/knetctl"}
+: ${REPO_NAME:="henderiw/knetctl"}
 : ${REPO_URL:="https://github.com/$REPO_NAME"}
 : ${PROJECT_URL:="https://docs.kubenet.dev"}
 : ${LATEST_URL:="https://api.github.com/repos/$REPO_NAME/releases/latest"}
